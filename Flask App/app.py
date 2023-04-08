f1afc16dca32fb7bb7a9d2fa194486e76105546b
Shashiraj Walsetwar <shashiraj.walsetwar@sikka.ai>
2023-04-07 23:26:19 -0700

    Final Update. Done for the day

--- Flask App/app.py
+++ Flask App/app.py
@@ -3,6 +3,7 @@ import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 import joblib
+import people_also_ask
 
 app = Flask(__name__)
 
@@ -32,9 +33,14 @@ def get_bot_response():
     scores = list(enumerate(cosine_similarity(query_vector, chatbot_object.question_vectors)[0]))
     sorted_scores = sorted(scores, key=lambda x: x[1], reverse=True)
     response = chatbot_object.data['answer'][sorted_scores[0][0]]
-    if sorted_scores[0][1] == 0:
-        return "I'm sorry, I don't understand."
+    if sorted_scores[0][1] < 0.50:
+        if people_also_ask.get_answer(user_message)['has_answer'] == True and len(people_also_ask.get_answer(user_message)['response']) > 0:
+            response_from_google = people_also_ask.get_answer(user_message)['response']
+            return response_from_google
+        else:
+            return 'Sorry, I do not have answer for this question. Could you be more Specific?'
     else:
+        print(f'Found Similarity: {sorted_scores[0][1]}')
         return response
     
 if __name__ == "__main__":
